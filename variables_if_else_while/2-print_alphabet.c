ef57239ae56735cce36255c0d1cfeacb5451b423
luisfe05 <lgonzalez05@gmail.com>
2026-02-15 21:10:29 -0400

    update task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
