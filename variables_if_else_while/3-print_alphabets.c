24d2a52d1419c8fbce9cfda8254a9045846585f3
luisfe05 <lgonzalez05@gmail.com>
2026-02-15 21:23:53 -0400

    update task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print lowercase and uppercase alphabets
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
