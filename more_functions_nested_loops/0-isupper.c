4841c98d633281557855df7456b62da6001d27ff
luisfe05 <lgonzalez05@gmail.com>
2026-02-22 00:00:23 -0400

    update task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - checks the code
+ * _isupper - checks for uppercase character
+ * @c: character to check
  *
- *
- * Return: 1 if uppercase, 0 otherwise
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
