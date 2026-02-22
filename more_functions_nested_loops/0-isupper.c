8f962d62f0f3dee84f16c8785c1a86f474aa9fc2
luisfe05 <lgonzalez05@gmail.com>
2026-02-21 23:54:55 -0400

    upload task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# More Functions, More Nested Loops
+
+Task 0
