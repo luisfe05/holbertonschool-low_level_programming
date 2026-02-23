ac9dce6d75336bc1eb15eca2dd77845713ce444a
luisfe05 <lgonzalez05@gmail.com>
2026-02-22 23:35:16 -0400

    upload task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: pointer to the string
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	length--;
+
+	while (length >= 0)
+	{
+		_putchar(s[length]);
+		length--;
+	}
+
+	_putchar('\n');
+}
