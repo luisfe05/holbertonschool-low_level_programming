77a4b0ff56bab1fca20d3336ae5cb31e181a82a8
luisfe05 <lgonzalez05@gmail.com>
2026-02-22 23:41:08 -0400

    upload task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string
+ *
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int i;
+	char temp;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+	}
+}
