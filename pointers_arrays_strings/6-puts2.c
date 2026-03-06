e950789d9c4069fa081152559073a3dd5440961a
luisfe05 <lgonzalez05@gmail.com>
2026-03-06 17:51:57 -0400

    upload task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string, starting with the first character, followed by a new line.
+ * puts2 - Prints every other character of a string
  * @str: The string to print from
  *
  * Return: void
