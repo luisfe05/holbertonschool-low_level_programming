4dbaf11b096a72c5a951d0de1b9f639fb89429c0
luisfe05 <lgonzalez05@gmail.com>
2026-04-15 19:06:49 -0400

    Upload task 0 memset Write a function that fills memory with a constant byte and update main.h with prototype

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: the memory area to fill
+ * @b: the constant byte to fill with
+ * @n: the number of bytes to fill
+ *
+ * Return: pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+
+	/* Fill the first n bytes of s with the byte b */
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
