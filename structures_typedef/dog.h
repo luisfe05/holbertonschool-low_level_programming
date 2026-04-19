513f622bd338684fb531423536abcbd13a5864ec
luisfe05 <lgonzalez05@gmail.com>
2026-04-19 18:05:12 -0400

    Upload task 3 Add typedef dog_t to dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,18 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdlib.h>
+
 /**
  * struct dog - structure representing a dog
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the name of the owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
