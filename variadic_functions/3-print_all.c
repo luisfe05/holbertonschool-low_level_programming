b4269435f77614bbf0cb5193f3b6a8c541e9918d
Luis Felipe Gonzalez <lgonzalez05@gmail.com>
2026-04-21 15:51:36 -0400

    Upload task 3 print_all Write a function that prints anything and update variadic_functions.h

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -12,43 +12,45 @@ void print_all(const char * const format, ...)
 	va_list args;
 	unsigned int i;
 	int printed;
+	unsigned int j;
 	char c;
 	int n;
-	float f;
+	double f;
 	char *s;
+	char *types;
 
 	va_start(args, format);
+	types = "cifs";
 	i = 0;
 	printed = 0;
 
 	/* Walk through each character in format */
 	while (format && format[i])
 	{
-		s = NULL;
-		if (format[i] == 'c')
-		{
-			c = va_arg(args, int);
-			printf("%s%c", printed ? ", " : "", c);
-			printed++;
-		}
-		else if (format[i] == 'i')
-		{
-			n = va_arg(args, int);
-			printf("%s%d", printed ? ", " : "", n);
-			printed++;
-		}
-		else if (format[i] == 'f')
-		{
-			f = va_arg(args, double);
-			printf("%s%f", printed ? ", " : "", f);
-			printed++;
-		}
-		else if (format[i] == 's')
-		{
-			s = va_arg(args, char *);
-			printf("%s%s", printed ? ", " : "", s ? s : "(nil)");
-			printed++;
-		}
+		/* Find which index matches: 0=c, 1=i, 2=f, 3=s, 4=no match */
+		j = 0;
+		while (j < 4 && format[i] != types[j])
+			j++;
+
+		/* Read the right argument only if we have a match */
+		c = (j == 0) ? (char)va_arg(args, int) : 0;
+		n = (j == 1) ? va_arg(args, int) : 0;
+		f = (j == 2) ? va_arg(args, double) : 0;
+		s = (j == 3) ? va_arg(args, char *) : NULL;
+
+		/* Print separator before value if not first */
+		if (j < 4)
+			printf("%s", printed++ ? ", " : "");
+
+		/* Print the value based on which type matched */
+		if (j == 0)
+			printf("%c", c);
+		if (j == 1)
+			printf("%d", n);
+		if (j == 2)
+			printf("%f", f);
+		if (j == 3)
+			printf("%s", s ? s : "(nil)");
 		i++;
 	}
 
