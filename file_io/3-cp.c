a54f490aa9e9d943d2238f1f1fa9d2c3f99e1428
Luis Felipe Gonzalez <lgonzalez05@gmail.com>
2026-04-22 20:52:31 -0400

    Add final README.md and task 3 cp Write a program that copies file content and update main.h

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,6 +18,41 @@ void close_fd(int fd)
 }
 
 /**
+ * copy_content - reads from fd_from and writes to fd_to
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @file_from: source file name for error messages
+ * @file_to: destination file name for error messages
+ */
+void copy_content(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+	char buf[1024];
+	ssize_t r;
+	ssize_t w;
+
+	while ((r = read(fd_from, buf, 1024)) > 0)
+	{
+		w = write(fd_to, buf, r);
+		if (w != r)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", file_to);
+			close_fd(fd_from);
+			close_fd(fd_to);
+			exit(99);
+		}
+	}
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", file_from);
+		close_fd(fd_from);
+		close_fd(fd_to);
+		exit(98);
+	}
+}
+
+/**
  * main - copies content of a file to another file
  * @argc: number of arguments
  * @argv: array of arguments
@@ -26,9 +61,8 @@ void close_fd(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	char buf[1024];
-	ssize_t r, w;
+	int fd_from;
+	int fd_to;
 
 	if (argc != 3)
 	{
@@ -38,7 +72,8 @@ int main(int argc, char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
@@ -48,24 +83,7 @@ int main(int argc, char *argv[])
 		close_fd(fd_from);
 		exit(99);
 	}
-	while ((r = read(fd_from, buf, 1024)) > 0)
-	{
-		w = write(fd_to, buf, r);
-		if (w != r)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close_fd(fd_from);
-			close_fd(fd_to);
-			exit(99);
-		}
-	}
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close_fd(fd_from);
-		close_fd(fd_to);
-		exit(98);
-	}
+	copy_content(fd_from, fd_to, argv[1], argv[2]);
 	close_fd(fd_from);
 	close_fd(fd_to);
 	return (0);
