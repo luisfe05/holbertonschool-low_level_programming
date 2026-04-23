61f8ac830bdd2c94eaf190f52fa58585d3c13d48
Luis Felipe Gonzalez <lgonzalez05@gmail.com>
2026-04-22 20:38:52 -0400

    Add final README.md and task 3 cp Write a program that copies file content and update main.h

---
+++ file_io/3-cp.c
@@ -0,0 +1,83 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * close_fd - closes a file descriptor and exits if it fails
+ * @fd: the file descriptor to close
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - copies content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from;
+	int fd_to;
+	char buf[1024];
+	ssize_t r;
+	ssize_t w;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	/* Open file_from for reading */
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	/* Open file_to for writing - create or truncate - rw-rw-r-- */
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close_fd(fd_from);
+		exit(99);
+	}
+
+	/* Read 1024 bytes at a time and write to file_to */
+	while ((r = read(fd_from, buf, 1024)) > 0)
+	{
+		w = write(fd_to, buf, r);
+		if (w != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close_fd(fd_from);
+			close_fd(fd_to);
+			exit(99);
+		}
+	}
+
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close_fd(fd_from);
+		close_fd(fd_to);
+		exit(98);
+	}
+
+	close_fd(fd_from);
+	close_fd(fd_to);
+
+	return (0);
+}
--- file_io/README.md
+++ file_io/README.md
@@ -9,6 +9,9 @@ to create, open, close, read and write files in C programming.
 | File | Description |
 | --- | --- |
 | `0-read_textfile.c` | Function that reads a text file and prints it to stdout |
+| `1-create_file.c` | Function that creates a file with given content |
+| `2-append_text_to_file.c` | Function that appends text at the end of a file |
+| `3-cp.c` | Program that copies the content of a file to another file |
 
 ## Author
 Luis Gonzalez - Holberton School San Juan, Puerto Rico
